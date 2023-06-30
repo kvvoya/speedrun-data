6ee27fbca2b9b6a72d9520cc714af59afd6d1b39
kvvoya <suruhakken@gmail.com>
2023-06-30 13:54:49 +0300

    some stuff

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const puppeteer = require('puppeteer');
-const readline = require('readline');
+import puppeteer from 'puppeteer';
+import readline from 'readline';
+import chalk from 'chalk';
+import clear from 'clear';
+
+const validUrlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
 
 async function scrapeCategoryLinks(page, link) {
+
    await page.goto(link);
 
    const categoryLinks = await page.$$eval('a', elements => elements.map(el => el.href));
@@ -14,9 +19,19 @@ async function scrapeCategoryLinks(page, link) {
    return filteredCategoryLinks;
 }
 
+function filterToLevelLinks(links) {
+   const filteredLevelLinks = Array.from(new Set(links.filter(link => link.includes('/level/'))));
+   console.log('Level links:', filteredLevelLinks);
+   return filteredLevelLinks;
+}
+
 async function scrapeWebsite(link) {
 
-   const browser = await puppeteer.launch();
+   if (!validUrlRegex.test(link)) {
+      console.log(chalk.red('INVALID LINK:', link));
+      return;
+   }
+
    const page = await browser.newPage();
 
    await page.goto(link);
@@ -24,8 +39,15 @@ async function scrapeWebsite(link) {
    const title = await page.title();
    console.log('Title:', title);
 
+   const leaderboardElement = await page.$('#leaderboard-dropdown');
+   if (!leaderboardElement) {
+      console.log(chalk.red('INVALID LINK (MUST BE A GAME LEADERBOARD):', link));
+      return;
+   }
+
    const links = await page.$$eval('a', elements => elements.map(el => el.href));
    const filteredLinks = Array.from(new Set(links.filter(link => link.includes('?h'))));
+   const levelLinks = filterToLevelLinks(links);
 
    const categoryLinksSet = new Set();
 
@@ -38,8 +60,12 @@ async function scrapeWebsite(link) {
 
    const categoryLinksArray = Array.from(categoryLinksSet);
    console.log('Category links:', categoryLinksArray);
+}
 
-   await browser.close();
+function displayWelcomeMessage() {
+   clear();
+   console.log(chalk.yellow.bold('Speedrun Data v0.1.0'));
+   console.log(chalk.green('by kvvoya'));
 }
 
 const r1 = readline.createInterface({
@@ -47,6 +73,13 @@ const r1 = readline.createInterface({
    output: process.stdout
 });
 
+
+displayWelcomeMessage();
+
+const browserOptions = {
+   headless: 'old'
+};
+const browser = await puppeteer.launch(browserOptions);
 r1.question('Enter a speedrun.com link to scrape: ', async (link) => {
    await scrapeWebsite(link);
    r1.close();
